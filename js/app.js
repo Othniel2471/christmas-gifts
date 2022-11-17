df7da2e25a302903890bd09a61e52a8ba12ad39d
othniel2471 <othnielomaidu@gmail.com>
2022-11-17 16:14:02 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("input");
+let name = document.querySelector(".name");
+const giver = document.querySelector("h4");
+const form = document.querySelector("form").addEventListener("click", (e) => {
+  e.preventDefault();
+});
