9f870723d8a956fc28a8d92973c418a0b65b22a0
othniel2471 <othnielomaidu@gmail.com>
2022-11-17 16:45:39 +0100

    second commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 const input = document.querySelector("input");
-let name = document.querySelector(".name");
-const giver = document.querySelector("h4");
+let partner = document.querySelector(".name");
+let error = document.querySelector(".error");
+const giver = document.querySelector(".givers-name");
 const form = document.querySelector("form").addEventListener("click", (e) => {
   e.preventDefault();
 });
+
+const query = () => {
+  if (input.value === "bernice") {
+    partner.innerText = "idris";
+    giver.innerText = `Hey ${input.value}`;
+  } else if (input.value === "idris") {
+    partner.innerText = "kachuwa";
+    giver.innerText = `Hey ${input.value}`;
+  } else if (input.value === "kachuwa") {
+    partner.innerText = "mummy";
+    giver.innerText = `Hey ${input.value}`;
+  } else if (input.value === "simon") {
+    partner.innerText = "othniel";
+    giver.innerText = `Hey ${input.value}`;
+  } else if (input.value === "mummy") {
+    partner.innerText = "hello mummy you're getting a gift for simon";
+  } else if (input.value === "othniel") {
+    partner.innerText = "hello othniel you're getting bernice a gift";
+    giver.innerText = `Hey ${input.value}`;
+  } else {
+    error.innerText = `You're not part of the family ${input.value}`;
+    input.value = "";
+    partner.innerText = "";
+  }
+};
+document.querySelector(".btn").addEventListener("click", query);
