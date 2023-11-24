13a9e5de955bf8db56f0606a5805887259dc7c88
othniel2471 <othnielomaidu@gmail.com>
2023-11-24 20:21:21 +0100

    updated app

--- js/app.js
+++ js/app.js
@@ -1,47 +1,55 @@
-const input = document.querySelector("#names");
-const enter = document.querySelector(".label");
-let partner = document.querySelector(".name");
-let error = document.querySelector(".alert");
-const giver = document.querySelector(".givers-name");
-const clear = document.querySelector(".clear-btn");
-const form = document.querySelector("form").addEventListener("click", (e) => {
+const input = document.querySelector('#names');
+const enter = document.querySelector('.label');
+let partner = document.querySelector('.name');
+let error = document.querySelector('.alert');
+const giver = document.querySelector('.givers-name');
+const clear = document.querySelector('.clear-btn');
+const form = document.querySelector('form').addEventListener('click', (e) => {
   e.preventDefault();
 });
 
 const query = () => {
   removeLabel();
-  if (input.value === "bernice") {
-    partner.innerText = "hello bernice you're getting idris a gift";
-    displayAlert("Match made", "success");
+  if (input.value === 'othniel') {
+    partner.innerText = `hello ${input.value} you're paired with mummy`;
+    displayAlert('Match made', 'success');
     giver.innerText = `Hey ${input.value}`;
-  } else if (input.value === "idris") {
-    partner.innerText = "hello idris you're getting kachuwa  a gift";
-    displayAlert("Match made", "success");
+  } else if (input.value === 'kezia') {
+    partner.innerText = `hello ${input.value} you're paired with idris`;
+    displayAlert('Match made', 'success');
     giver.innerText = `Hey ${input.value}`;
-  } else if (input.value === "kachuwa") {
-    partner.innerText = "hello kachuwa you're getting mummy  a gift";
-    displayAlert("Match made", "success");
+  } else if (input.value === 'bernice') {
+    partner.innerText = `hello ${input.value} you're paired with othniel`;
+    displayAlert('Match made', 'success');
     giver.innerText = `Hey ${input.value}`;
-  } else if (input.value === "simon") {
-    partner.innerText = "hello simon you're getting othniel a gift";
-    displayAlert("Match made", "success");
+  } else if (input.value === 'kachuwa') {
+    partner.innerText = `hello ${input.value} you're paired with simon`;
+    displayAlert('Match made', 'success');
     giver.innerText = `Hey ${input.value}`;
-  } else if (input.value === "mummy") {
-    partner.innerText = "hello mummy you're getting a gift for simon";
-    displayAlert("Match made", "success");
-  } else if (input.value === "othniel") {
-    partner.innerText = "hello othniel you're getting bernice a gift";
-    displayAlert("Match made", "success");
+  } else if (input.value === 'mummy') {
+    partner.innerText = `hello ${input.value} you're paired with kachuwa`;
+    displayAlert('Match made', 'success');
     giver.innerText = `Hey ${input.value}`;
-  } else if (input.value === "") {
-    displayAlert("please enter name", "danger");
+  } else if (input.value === 'simon') {
+    partner.innerText = `hello ${input.value} you're paired with lois`;
+    displayAlert('Match made', 'success');
+    giver.innerText = `Hey ${input.value}`;
+  } else if (input.value === 'idris') {
+    partner.innerText = `hello ${input.value} you're paired with kezia`;
+    displayAlert('Match made', 'success');
+    giver.innerText = `Hey ${input.value}`;
+  } else if (input.value === 'lois') {
+    partner.innerText = `hello ${input.value} you're paired with bernice`;
+    displayAlert('Match made', 'success');
+    giver.innerText = `Hey ${input.value}`;
   } else {
-    displayAlert(`You're not part of the family ${input.value}`, "danger");
+    displayAlert(`You're not part of the family ${input.value}`, 'danger');
     // error.innerText = `You're not part of the family ${input.value}`;
-    input.value = "";
-    partner.innerText = "";
-    giver.innerText = "";
+    input.value = '';
+    partner.innerText = '';
+    giver.innerText = '';
   }
+  input.value = '';
 };
 
 // displaying alert
@@ -51,24 +59,24 @@ const displayAlert = (text, action) => {
 
   // remove alert
   setTimeout(() => {
-    error.textContent = "";
+    error.textContent = '';
     error.classList.remove(`alert-${action}`);
   }, 1000);
 };
 
 // remove label
 const removeLabel = () => {
-  if (input.value !== "") {
-    enter.innerText = "";
+  if (input.value !== '') {
+    enter.innerText = '';
   }
 };
 
 // clear fields
 const reset = () => {
-  input.value = "";
-  partner.innerText = "";
-  giver.innerText = "";
+  input.value = '';
+  partner.innerText = '';
+  giver.innerText = '';
 };
 
-document.querySelector(".btn").addEventListener("click", query);
-clear.addEventListener("click", reset);
+document.querySelector('.btn').addEventListener('click', query);
+clear.addEventListener('click', reset);
